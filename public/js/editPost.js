fe770bed0771a1a5bbbeef41f305ab93f0799dda
Jbarbss <jbarbanel@gmail.com>
2021-05-13 12:39:00 -0400

    added coce for js files in public folder (comment, createPost, deletePost, editPost, login, logout, and signup

---
+++ public/js/editPost.js
@@ -0,0 +1,28 @@
+const editPostHandler = async (event) => {
+    event.preventDefault();
+
+    const post_title = document.querySelector('input[name="post-title"]').value;
+    const post_body = document.querySelector('input[name="post-body"]').value.trim();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            post_title,
+            post_body
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editPostHandler);
